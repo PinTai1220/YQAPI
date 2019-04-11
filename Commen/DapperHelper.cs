8da6faf5c570a3057bc21dbc6fc3138b69d26b36
bitType <2386690386@qq.com>
2019-04-11 09:55:45 +0800

    44

---
+++ Commen/DapperHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace dapper1
+{
+    public class DapperHelper<T>
+    {
+        private IDbConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlDiagnosticsDb"].ConnectionString);
+        public int Execute(string SqlStr,object Obj)
+        {
+           return conn.Execute(SqlStr, Obj);
+        }
+        public IEnumerable<T> Query(string SqlStr,object Obj)
+        {
+            return conn.Query<T>(SqlStr, Obj);
+        }
+    }
+}
